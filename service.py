ed6181d4e42eaf7952f0bee15451a50bec15daac
Peter K <pkjar16@student.aau.dk>
2020-10-15 11:52:24 +0200

    #5 done, #1 done and #3 done

--- service.py
+++ service.py
@@ -1,31 +1,31 @@
 import json
 import base64
-import hashlib 
+import hashlib
 
 # Used the generate a JSON object for the configuration file
 def generate_config_object():
-    cfgDict = {} 
+    cfgDict = {}
     cfgDict["cluster"] = "test"
-    cfgDict["neighbours"] = ["127.0.0.1", "127.0.0.2", "127.0.0.3"] 
-  
-    json_object = json.dumps(cfgDict, indent = 4)   
+    cfgDict["neighbours"] = ["127.0.0.1", "127.0.0.2", "127.0.0.3"]
+
+    json_object = json.dumps(cfgDict, indent = 4)
     print(json_object)
     return(json_object)
 
-# Inputs a JSON objects and output the MD5 equivalent hash version of it
+# Inputs a JSON objects and output the SHA256 equivalent hash version of it
 def hash_json(json_object):
-    enc = json_object.encode() 
+    enc = json_object.encode()
     b64 = base64.encodebytes(enc)
 
-    result = hashlib.md5(b64) 
+    result = hashlib.sha256(b64) 
   
-    print("The MD5 hexadecimal equivalent of hash is : ", end ="") 
-    print(result.hexdigest()) 
+    print("The SHA256 hexadecimal equivalent of hash is : ", end ="") 
+    print(result.hexdigest())
     return(result.hexdigest())
 
 # Take a JSON object as input and generates a configuration file
 def generate_config_file(json_object):
-    with open("network.json", "w") as outfile: 
+    with open("network.json", "w") as outfile:
         outfile.write(json_object)
 
 json_object = generate_config_object()
