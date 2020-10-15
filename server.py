36d4210b56d009a82018897bfca796100d2bff8c
Peter K <pkjar16@student.aau.dk>
2020-10-15 13:58:58 +0200

    #2 done - Added config file to gitignore

--- server.py
+++ server.py
@@ -13,7 +13,19 @@ def update_valid_hash(new_hash):
     print("Updated Hash to " + str(new_hash))
 
 def update_config_file(config):
+    with open("network.json", "w") as outfile:
+        outfile.write(config)
     print("Updated Configuration File")
+
+def hash_json(json_object):
+    enc = json_object.encode()
+    b64 = base64.encodebytes(enc)
+
+    result = hashlib.sha256(b64)
+
+    print("The SHA256 hexadecimal equivalent of hash is : ", end ="")
+    print(result.hexdigest())
+    return(result.hexdigest())
 
 @app.route('/updateConfigFile', methods=['GET','POST'])
 def get_config_file():
