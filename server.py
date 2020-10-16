20150d2159e2fa50407645755939daca2dfd1b06
Peter K <pkjar16@student.aau.dk>
2020-10-17 00:35:37 +0200

    Removed all prints

--- server.py
+++ server.py
@@ -8,23 +8,17 @@ def same_version(new_hash):
     return False
 
 def update_valid_hash(new_hash):
-    print("My current hash is " + str(valid_hash))
     globals()['valid_hash'] = new_hash
-    print("Updated Hash to " + str(new_hash))
 
 def update_config_file(config):
     with open("network.json", "w") as outfile:
         outfile.write(config)
-    print("Updated Configuration File")
 
 def hash_json(json_object):
     enc = json_object.encode()
     b64 = base64.encodebytes(enc)
 
     result = hashlib.sha256(b64)
-
-    print("The SHA256 hexadecimal equivalent of hash is : ", end ="")
-    print(result.hexdigest())
     return(result.hexdigest())
 
 @app.route('/updateConfigFile', methods=['GET','POST'])
